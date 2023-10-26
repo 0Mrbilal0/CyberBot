04eb7968bf0a51c3b4e1d356f9f193e3e0fddf84
Mrbilal <bilal_itchir@Outlook.fr>
2023-10-26 17:01:22 +0200

    Finish commands "help"

--- commands/utilitaire/help.js
+++ commands/utilitaire/help.js
@@ -1,10 +1,36 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Affichage des commands'),
 
     async execute(interaction) {
+        const commandsObj = require('../../commands')
+        let commands = [];
         
+
+        for (const key in commandsObj) {
+            commands.push({name: commandsObj[key].name, description: commandsObj[key].description})
+        }
+        let commandList = ['','']
+        commands.forEach(element => {
+            commandList[0] += element.name+'\n'
+            commandList[1] += element.description+'\n'
+        });
+        console.log(typeof commandList)
+        const embed = new EmbedBuilder()
+            .setTitle('Liste des commandes')
+            .setColor('DarkBlue')
+            .setFields(
+                { name: 'Name', value: commandList[0], inline: true },
+                { name: 'Description', value: commandList[1], inline: true }
+            )
+
+        interaction.reply({
+            content: '',
+            embeds: [embed],
+            ephemeral: true
+        })
     }
 }
